b413c1732516686aaa4ae49e1cc514b0c4c040ba
Ivan A. Filatoff <fill@MacBookAir.local>
2025-10-12 13:05:08 +0300

    fix

--- yandex_infra/output.tf
+++ yandex_infra/output.tf
@@ -6,14 +6,14 @@ output "nexus01_internal" {
   value = yandex_compute_instance.nexus01.network_interface.0.ip_address
 }
 
-# output "app01" {
-#   value       = yandex_compute_instance.app01.network_interface.0.ip_address
-# }
+output "app01" {
+  value       = yandex_compute_instance.app01.network_interface.0.ip_address
+}
 
 output "db01" {
   value       = yandex_compute_instance.db01.network_interface.0.ip_address
 }
 
-# output "runner01" {
-#   value       = yandex_compute_instance.runner01.network_interface.0.ip_address
-# }
+output "runner01" {
+  value       = yandex_compute_instance.runner01.network_interface.0.ip_address
+}
